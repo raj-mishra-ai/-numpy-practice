9b4d8ca31c0514c43963d569ee1186ab94fd74ad
Raj Mishra <rajmishra.ai01@gmail.com>
2026-06-03 00:01:32 +0530

    Updated NumPy lecture 3

--- numpy_demo.py
+++ numpy_demo.py
@@ -28,7 +28,8 @@ result = timeit.timeit(
 print(result)
 
 #Lecture 3 (creating numpy arrays)
-# Array
+#  Type of Array
+#1d array
 import numpy as np
 
 x = [1,2,3,4]
@@ -36,7 +37,25 @@ y = np.array([1,2,3,4])
 
 print(y)
 print(type(y))
+print(y.ndim)
 
+#2d array
+ar2=np.array([[1,2,3,4,],[1,2,3,4,]])
+print(ar2)
+print(ar2.ndim)
+
+#3d array
+ar3=np.array ([[[1,2,3,4,],[1,2,3,4,],[1,2,3,4]]])
+print(ar3)
+print(ar3.ndim)
+
+#multidim array
+import numpy as np
+
+arn = np.array([1,2,3,4], ndmin=20)
+
+print(arn)
+print(arn.ndim)
 
 
 
